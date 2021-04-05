5851e308e4fba64e40688688496a2755c70ae830
Rodrigo H <46557955+rodhd@users.noreply.github.com>
2021-04-05 18:02:41 +0200

    added nswag

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -29,7 +29,7 @@ namespace Api
         {
             services.AddControllers();
             services.AddFaunaDb();
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Api", Version = "v1"}); });
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,8 +38,8 @@ namespace Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
+                app.UseOpenApi();
+                app.UseSwaggerUi3();
             }
 
             app.UseHttpsRedirection();
